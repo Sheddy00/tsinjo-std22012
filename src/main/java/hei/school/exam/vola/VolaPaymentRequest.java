803dec4df0433306b1a0a3478b3335c570040d94
Sheddy <hei.sheddy.2@gmail.com>
2025-08-05 16:27:58 +0300

    feat: vola class required

--- src/main/java/hei/school/exam/vola/VolaPaymentRequest.java
+++ src/main/java/hei/school/exam/vola/VolaPaymentRequest.java
@@ -1,4 +1,11 @@
 package hei.school.exam.vola;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class VolaPaymentRequest {
+  private String method;
+  private Long amount;
 }
--- src/main/java/hei/school/exam/vola/VolaPaymentResponse.java
+++ src/main/java/hei/school/exam/vola/VolaPaymentResponse.java
@@ -1,4 +1,9 @@
 package hei.school.exam.vola;
 
+import lombok.Getter;
+
+@Getter
 public class VolaPaymentResponse {
+  private String id;
+  private String status;
 }
